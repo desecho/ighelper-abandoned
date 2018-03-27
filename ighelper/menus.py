ffa29b06c24f211dc3f6f3bcdc274ae90cfb0808
Anton Samarchyan <desecho@gmail.com>
2018-03-27 07:50:48 -0400

    Change item order in menu

--- ighelper/menus.py
+++ ighelper/menus.py
@@ -10,7 +10,7 @@ def is_authenticated(request):
 
 
 Menu.add_item('main', MenuItem(_('Home'), reverse('home'), check=is_authenticated))
-Menu.add_item('main', MenuItem(_('Followers'), reverse('followers'), check=is_authenticated))
 Menu.add_item('main', MenuItem(_('Medias'), reverse('medias'), check=is_authenticated))
 Menu.add_item('main', MenuItem(_('Likes'), reverse('likes'), check=is_authenticated))
+Menu.add_item('main', MenuItem(_('Followers'), reverse('followers'), check=is_authenticated))
 Menu.add_item('main', MenuItem(_('Following'), reverse('followed'), check=is_authenticated))
